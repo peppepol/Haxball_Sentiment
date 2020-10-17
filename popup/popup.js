e2ba4f62af680a56aeb24d1d84f7cfcbddf8ec13
peppepol <peppevita3@yahoo.it>
2020-10-17 19:20:25 +0200

    First version ready to release

--- popup/popup.js
+++ popup/popup.js
@@ -1,59 +1,82 @@
 //START
 browser.storage.local.get("textarea").then( res => {
-    document.getElementsByTagName("textarea")[0].innerText = JSON.stringify(res.textarea);
+   
+    document.getElementsByTagName("textarea")[0].innerHTML = JSON.stringify(res.textarea, null, 1);
     try{
+        if(res.textarea === undefined){
+            document.getElementsByTagName("textarea")[0].innerText = "{}";
+            throw new Error("JSON Undefined");
+        }
         JSON.stringify(res.textarea);
         document.getElementById("error").innerText = "Oggetto JSON valido!"
     }catch(err){
-        document.getElementById("error").innerText = "Oggetto JSON non valido!"
+        if(err.message === "JSON Undefined")
+            document.getElementById("error").innerText = "";
+        else
+            document.getElementById("error").innerText = "Oggetto JSON non valido!";
     }
 });
-
-browser.storage.onChanged.addListener((changes, areaName)=>{
-    console.log("Local Storage aggiornato correttamente!");
-})
 
 browser.storage.local.get("extStatus").then( res => {
     document.getElementById("extStatus").innerText = (res.extStatus ? "ESTENSIONE ATTIVA!": "");
 });
 
+//STORAGE CHANGED EVENT
+browser.storage.onChanged.addListener((changes, areaName)=>{
+    if ('textarea' in changes){
+        console.log("\n\nOLD VALUE");
+        console.log(changes.textarea.oldValue);
+        console.log("NEW VALUE");
+        console.log(changes.textarea.newValue);
+        
+        if(changes.textarea.newValue === undefined)
+            document.getElementsByTagName("textarea")[0].innerText = "{}";
+    }
+});
+
+//TAB IN TEXTAREA
+document.getElementsByClassName('textarea')[0].addEventListener('keydown', function(e) {
+    if (e.key == 'Tab') {
+      e.preventDefault();
+      var start = this.selectionStart;
+      var end = this.selectionEnd;
+  
+      // set textarea value to: text before caret + tab + text after caret
+      this.value = this.value.substring(0, start) +
+      " " + this.value.substring(end);
+  
+      // put caret at right position again
+      this.selectionStart =
+        this.selectionEnd = start + 1;
+    }
+  });
+
 /////////////////////////////////////////////////////////////
 
+/*
+* Listener for clicks event
+*/
 function listenForClicks() {
-    const textarea = document.getElementsByTagName("textarea")[0];
-    textarea.addEventListener('input',(e)=>{
-        let message;
-        try{
-            message = JSON.parse(textarea.value);
-            browser.storage.local.set({
-                textarea: message
-            })  
-                .then(()=>{
-                    document.getElementById("error").innerText = "Oggetto JSON valido!";
-                })
-        }catch(err){
-            document.getElementById("error").innerText = "Oggetto JSON non valido!";
-        }        
-    })
 
     document.addEventListener("click", (e) => {
-        //console.log("Ho ricevuto un click!");
 
+        /*
+        * Enable web extension
+        */
         const enable = (tabs) => {
-            
             browser.storage.local.set({
                 extStatus: true
             });
             browser.tabs.sendMessage(tabs[0].id, {
                 command: 'enable'
             });
-
             document.getElementById("extStatus").innerText = "ESTENSIONE ATTIVA";
         }
         
-
+        /*
+        * Disable web extension
+        */
         const disable = (tabs) => {
-            
             browser.storage.local.set({
                 extStatus: false
             });
@@ -63,17 +86,14 @@ function listenForClicks() {
             document.getElementById("extStatus").innerText = "";
         }
 
-        /**
-        * Just log the error to the console.
+        /*
+        * Report generic error in console
         */
         function reportError(error) {
             console.error(`Error report: ${error}`);
         }
 
-        /**
-        * Get the active tab,
-        * then call "beastify()" or "reset()" as appropriate.
-        */
+    
         if (e.target.classList.contains("btn-enable")) {
             browser.tabs.query({active: true, currentWindow: true})
                 .then(enable)
@@ -82,6 +102,34 @@ function listenForClicks() {
             browser.tabs.query({active: true, currentWindow: true})
                 .then(disable)
                 .catch(reportError);
+        }else if(e.target.classList.contains("btn-set")){
+            try{
+                const textarea = document.getElementsByTagName("textarea")[0];
+                let message;
+                message = JSON.parse(textarea.value);
+                browser.storage.local.set({
+                    textarea: message
+                })  
+                    .then(()=>{
+                        document.getElementById("error").innerText = "Oggetto JSON valido!";
+                    });
+                browser.tabs.query({active:true, currentWindow: true})
+                    .then( (tabs)=>{
+                        browser.tabs.sendMessage(tabs[0].id,{
+                            command: 'update'
+                        });
+                    });
+            }catch(err){
+                document.getElementById("error").innerText = "Oggetto JSON non valido!";
+            }
+        }else if(e.target.classList.contains("btn-clear")){
+            browser.storage.local.remove("textarea");
+            browser.tabs.query({active:true, currentWindow: true})
+                    .then( (tabs)=>{
+                        browser.tabs.sendMessage(tabs[0].id,{
+                            command: 'clear'
+                        });
+                    });
         }
     }
 )};
@@ -95,8 +143,7 @@ function reportExecuteScriptError(error) {
 }
 
 /**
-* When the popup loads, inject a content script into the active tab,
-* and add a click handler.
+* When the popup loads, inject a content script into the active tab.
 * If we couldn't inject the script, handle the error.
 */
 browser.tabs.executeScript({file: "/scripts/content.js"})
--- scripts/content.js
+++ scripts/content.js
@@ -6,10 +6,15 @@
 
     window.hasRun = true;
 
+    //GLOBAL VARIABLES
     var obj;
-    browser.storage.local.get("textarea").then(r => obj=r.textarea);
-
     let keys = ["5","6","7","8","9","0"];
+    let arrow = ["ArrowLeft","ArrowUp","ArrowRight","ArrowDown"];
+
+
+    /*
+    * Handle any key pressed
+    */
     function handleKey(e){
       //console.log(e.key);
       //console.log(obj);
@@ -18,16 +23,17 @@
       let inp = frame.getElementsByClassName('input')[0].children[0];
       let send = frame.getElementsByClassName('input')[0].children[1];
       
-      if(keys.includes(e.key)){
-        let command = "/avatar " + (obj[e.key] === undefined ? "" : obj[e.key]);
-
+      if(keys.includes(e.key) || arrow.includes(e.key) && (e.key in obj)){
+        let command = "/avatar " + (obj[e.key.toString()] === undefined ? "" : obj[e.key.toString()]);
         inp.value = command;
         send.click();
         delAvatarSet(frame);
       }
     }
 
-    //ELIMINAZIONE "Avatar set"
+    /*
+    * Delete "Avatar set" from chat after commnad send
+    */
     function delAvatarSet(frame){
       var notices = frame.body.getElementsByClassName("notice");
       for (var i = 0; i < notices.length; i++) {
@@ -37,19 +43,26 @@
         }
       }
     }
-  
+    
+    //LISTENER DEI MESSAGGI
     browser.runtime.onMessage.addListener((message) => {
       if (message.command === "enable") {
         let frame = document.getElementsByTagName('iframe')[0].contentWindow.document;
+        browser.storage.local.get("textarea").then(r => obj=r.textarea);
         if(frame !== undefined){
-          frame.addEventListener("keypress", handleKey)
+          frame.addEventListener("keydown", handleKey)
         }
         
-      } else if (message.command === "disable") {
+      }else if (message.command === "disable") {
         let frame = document.getElementsByTagName('iframe')[0].contentWindow.document;
         if(frame !== undefined){
-          frame.removeEventListener("keypress", handleKey)
+          frame.removeEventListener("keydown", handleKey)
         }
+      }else if (message.command === "update"){
+        browser.storage.local.get("textarea").then(r => obj=r.textarea);
+
+      }else if (message.command === "clear"){
+        browser.storage.local.get("textarea").then(r => obj=r.textarea);
       }
     });
   
